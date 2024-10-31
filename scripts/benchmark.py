d0f28e8c31148d9598a6c2388d9e5fb71c352b8d
Andrew Santhosh <andrewsanthosh264@gmail.com>
2024-11-01 00:23:37 +0530

    update benchmark script

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -28,12 +28,16 @@ dummy_cfg.hidden_size = 2560
 dummy_cfg.intermediate_size = 6912
 dummy_cfg.num_layers = 24
 dummy_cfg.num_kv_heads = 8
-dummy_cfg.max_seq_len = 2048
-batch_size = 2
+dummy_cfg.max_seq_len = 4096
+batch_size = 1
 ############################################################################################################
 
-MODEL_PARTS = {'attention': Attention(dummy_cfg, 0).to(device=device, dtype=dtype), 'mlp': MLP(dummy_cfg).to(device=device, dtype=dtype),
-    'transformer_block': Block(dummy_cfg, 0).to(device=device, dtype=dtype), 'full': LLM(dummy_cfg).to(device=device, dtype=dtype)}
+MODEL_PARTS = {
+    'attention': Attention(dummy_cfg, 0).to(device=device, dtype=dtype),
+    'mlp': MLP(dummy_cfg).to(device=device, dtype=dtype),
+    'transformer_block': Block(dummy_cfg, 0).to(device=device, dtype=dtype),
+    'full': LLM(dummy_cfg).to(device=device, dtype=dtype)
+}
 compiled = True
 if compiled:
     for k, model in MODEL_PARTS.items():
@@ -48,11 +52,14 @@ def benchmark_function(func: Callable, kwargs: Dict[str, Any], iterations: int =
     func(**kwargs)
 
     for _ in range(iterations):
-        start_time = time.perf_counter()
+        start_event = torch.cuda.Event(enable_timing=True)
+        end_event = torch.cuda.Event(enable_timing=True)
+        start_event.record()
         func(**kwargs)
+        end_event.record()
         torch.cuda.synchronize()
-        end_time = time.perf_counter()
-        execution_times.append((end_time - start_time) * 1000)  # Convert to milliseconds
+        exec_time = start_event.elapsed_time(end_event)
+        execution_times.append(exec_time)  # Convert to milliseconds
 
     sorted_times = sorted(execution_times)
     percentile_1 = np.percentile(execution_times, 1)
@@ -148,8 +155,7 @@ def rms_norm_inputs(device, dtype) -> dict:
             'normalized_shape': (dummy_cfg.hidden_size,), 'weight': torch.ones(dummy_cfg.hidden_size, dtype=dtype, device=device)}
 
 def model_inputs(device, dtype) -> dict:
-    return {'x': torch.randint(dummy_cfg.vocab_size, (batch_size, dummy_cfg.max_position_embeddings), device=device),
-            'attn_mask': torch.ones((batch_size, dummy_cfg.max_position_embeddings), device=device)}
+    return {'input_ids': torch.randint(dummy_cfg.vocab_size, (batch_size, dummy_cfg.max_position_embeddings), device=device)}
 
 def attn_inputs(device, dtype) -> dict:
     return {'x': torch.randn((batch_size, dummy_cfg.max_position_embeddings, dummy_cfg.hidden_size), dtype=dtype, device=device), 'freqs': (
@@ -167,11 +173,15 @@ def main():
     benchmark_tasks: Dict[Callable, Dict[str, Any]] = {
         torch.matmul: {'input': torch.randn((dummy_cfg.hidden_size, dummy_cfg.max_position_embeddings), dtype=dtype, device=device),
                        'other': torch.randn((batch_size, dummy_cfg.max_position_embeddings, dummy_cfg.vocab_size), dtype=dtype, device=device)},
-        flash_scaled_dot_product_attention: sdpa_inputs(device, dtype), mem_eff_scaled_dot_product_attention: sdpa_inputs(device, dtype),
-        cudnn_scaled_dot_product_attention: sdpa_inputs(device, dtype), F.rms_norm: rms_norm_inputs(device, dtype),
+        flash_scaled_dot_product_attention: sdpa_inputs(device, dtype),
+        mem_eff_scaled_dot_product_attention: sdpa_inputs(device, dtype),
+        cudnn_scaled_dot_product_attention: sdpa_inputs(device, dtype),
+        F.rms_norm: rms_norm_inputs(device, dtype),
 
-        attention_block: attn_inputs(device, dtype), mlp_block: transformer_block_inputs(device, dtype),
-        transformer_block: attn_inputs(device, dtype), full_model_forward: model_inputs(device, dtype)}
+        attention_block: attn_inputs(device, dtype),
+        mlp_block: transformer_block_inputs(device, dtype),
+        transformer_block: attn_inputs(device, dtype),
+        full_model_forward: model_inputs(device, dtype)}
 
     print("Starting benchmark suite...")
     for func, kwargs in benchmark_tasks.items():
